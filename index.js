3246af2dbb447110247c6465e610024222c982f3
aminahh02 <abiolaminah@gmail.com>
2025-02-05 13:56:51 +0100

    commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let messages = document.getElementById("message");
+let match = document.getElementById("sample");
+let boxes = document.querySelectorAll(".box");
+let randomColour = [];
+
+for (let i = 0; i < 6; i++) {
+    let randomColour1 = Math.floor(Math.random()*256);
+    let randomColour2 = Math.floor(Math.random()*256);
+    let randomColour3 = Math.floor(Math.random()*256);
+
+    let colourString = `rgb(${randomColour1}, ${randomColour2}, ${randomColour3})`;
+
+    randomColour.push(colourString);
+    console.log(colourString);
+
+    boxes[i].style.backgroundColor = colourString;   
+}
+
+let randomIndex = Math.floor(Math.random() * 6);
+match.style.backgroundColor = randomColour[randomIndex];
+
+let scoreDisplay = document.getElementById("scored");
+let score = localStorage.getItem("score") ? parseInt(localStorage.getItem("score")) : 0;
+scoreDisplay.innerText = score;
+
+let checkAnswer = (cardIndex) => {
+    if (randomColour[cardIndex] === match.style.backgroundColor) {
+        messages.innerHTML =  "<p> Yay! you got it right!</p>";
+        
+        score++;
+        localStorage.setItem("score", score);
+        scoreDisplay.innerText = score;
+
+        setTimeout(() => {
+            window.location.reload();
+        }, 1500)
+
+    } else {
+        messages.innerHTML = "<p> Aww! Try that again!</p>"
+    }
+}
+
+
+let newGame = document.getElementById("newgames")
+newGame.addEventListener ("click", () => {
+
+    localStorage.setItem("score", 0);
+    scoreDisplay.innerText = 0;
+
+    setTimeout(() => {
+        window.location.reload();
+    }, 500); 
+})
+
+
